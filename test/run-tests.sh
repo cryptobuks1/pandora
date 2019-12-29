f88ef6f0c5a74e55f5231827fade7c7664ebc8f6
lluuvr <23312691+lluuvr@users.noreply.github.com>
2019-12-29 16:48:18 +0300

    added TODO about tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -8,6 +8,7 @@ go test \
   -c -covermode=count -coverpkg ./pkg/...,./cmd/node \
   ./cmd/node/ || exit
 
+# TODO `|| true` here is incorrect but we need delete test binary and coverage file after tests
 go test -v -timeout 10m -count=1 ./test/node/ || true
 
 rm -rf ./test/node/coverage.out ./test/node/node.test || exit
